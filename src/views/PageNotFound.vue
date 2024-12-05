c7a5193e9830608a27d474ca55cc06cf50478ba9
Artem Ilinov <erizo4747@gmail.com>
2024-12-05 19:42:14 +0700

    delete PageNotFound images

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,12 +1,9 @@
-<script setup lang="ts">
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-<div class="content-container">
-<div class="error-image">
-</div>
-</div>
+  <div class="content-container">
+    <div class="error-image"></div>
+  </div>
 </template>
 
 <style scoped>
@@ -17,14 +14,14 @@
   justify-content: center;
   align-items: center;
 }
+
 .error-image {
   width: 100%;
   height: 100%;
-  background: url("@/assets/backgroung/404-black-version.png") top/contain no-repeat;
 }
+
 @media (prefers-color-scheme: dark) {
   .error-image {
-    background: url("@/assets/backgroung/404-light-version.png") top/contain no-repeat;
   }
 }
 </style>
